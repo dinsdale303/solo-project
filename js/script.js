f58519d981495b314116b338c1b6ff6ef2c2b0b5
Piotr Stefanowicz <piotrekstefanowicz@gmail.com>
2020-07-27 21:55:51 +0200

    Modify topbar

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 'use strict';
 
+var inactiveMenuLink = document.querySelectorAll('.menu-list-item');
+var inactivePageView = document.querySelectorAll('.view-page');
+
+/* remove highlight menu link */
+function removeLink() {
+	for (var i = 0; i < inactiveMenuLink.length; i++) {
+		inactiveMenuLink[i].classList.remove('link-change-view');
+	}
+}
+
+/* hide page */
+function removePage() {
+	for (var i = 0; i < inactivePageView.length; i++) {
+		inactivePageView[i].classList.remove('page-active');
+		inactivePageView[i].classList.add('page-inactive');
+	}
+}
+
+/* change displayed page and highlight menu link */
 var changeView = function (idLink, idView) {
-	var menuLink = document.getElementById(idLink);
-	var loadedPage = document.getElementById(idView);
-	var inactiveMenuLink = document.querySelectorAll('.menu-list-item');
-	var inactivePageView = document.querySelectorAll('.view-page');
+	var activeMenuLink = document.getElementById(idLink);
+	var activePageView = document.getElementById(idView);
 
-	function removeLink() {
-		for (var i = 0; i < inactiveMenuLink.length; i++) {
-			inactiveMenuLink[i].classList.remove('link-change-view');
-		}
-	}
 	removeLink();
-
-	function removePage() {
-		for (var i = 0; i < inactivePageView.length; i++) {
-			inactivePageView[i].classList.remove('page-active');
-			inactivePageView[i].classList.add('page-inactive');
-		}
-	}
 	removePage();
 
-	menuLink.classList.add('link-change-view');
-	loadedPage.classList.remove('page-inactive');
-	loadedPage.classList.add('page-active');
+	activeMenuLink.classList.add('link-change-view');
+	activePageView.classList.remove('page-inactive');
+	activePageView.classList.add('page-active');
 }
