f971fecaa04777ebc1d852b30436cf4e7c6b9003
Piotr Stefanowicz <piotrekstefanowicz@gmail.com>
2020-07-27 21:59:35 +0200

    Add general statistics and links

--- js/script.js
+++ js/script.js
@@ -29,4 +29,57 @@ var changeView = function (idLink, idView) {
 	activeMenuLink.classList.add('link-change-view');
 	activePageView.classList.remove('page-inactive');
 	activePageView.classList.add('page-active');
+
+	if (idView == 'viewGeneral') {
+		var showLinks = document.getElementById('viewLinks');
+		showLinks.classList.remove('page-inactive');
+		showLinks.classList.add('page-active');
+	}
 }
+
+/* chart */
+Chart.defaults.global.legend.labels.usePointStyle = true;
+var chartOne = document.getElementById('generalChart').getContext('2d');
+var chart = new Chart(chartOne, {
+    type: 'bar',
+    data: {
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        datasets: [{
+            label: "Signups",
+            backgroundColor: '#56819F',
+            borderColor: '#56819F',
+            data: [ 352, 191, 241, 254, 426, 356, 272, 259, 281, 100 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F58220',
+            borderColor: '#F58220',
+            data: [ 406, 172, 301, 220, 447, 111, 250, 504, 293, 300 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#04AE00',
+            borderColor: '#04AE00',
+            data: [ 359, 149, 268, 290, 467, 241, 213, 338, 248, 200 ],
+            hidden: true,
+        }]
+    },
+	options: {
+		legend: {
+			labels: {
+				fontSize: 15,
+				padding: 15,
+			}
+		},
+		scales: {
+			yAxes: [{
+				gridLines: {
+					display: false,
+				},
+				ticks: {
+					beginAtZero: true,
+				}
+			}]
+		}
+	}
+});
